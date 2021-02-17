9c02b829eca1a224953dcda0894c7e636b2d8b15
techaas <techaas@gmail.com>
2021-02-17 15:06:30 +0900

    Initial commit

---
+++ app/src/main/java/com/techaas/dev/android/simplevplayer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.techaas.dev.android.simplevplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
